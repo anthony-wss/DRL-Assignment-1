a9a033d6d718a6ea635b6e467af75a029e5f8aa8
Anthony <b09902033@csie.ntu.edu.tw>
2025-03-21 17:06:38 +0800

    simple baseline

--- simple_custom_taxi_env.py
+++ simple_custom_taxi_env.py
@@ -221,5 +221,5 @@ if __name__ == "__main__":
         "fuel_limit": 5000
     }
     
-    agent_score = run_agent("student_agent.py", env_config, render=True)
+    agent_score = run_agent("student_agent.py", env_config, render=False)
     print(f"Final Score: {agent_score}")
